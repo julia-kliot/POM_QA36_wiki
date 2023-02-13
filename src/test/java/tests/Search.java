46aedf05943b5b822b2624b3d6b6f5ec6dc3cea2
julia-kliot <juliakliot.jk@gmail.com>
2023-02-13 20:59:12 +0300

    launch+research

---
+++ src/test/java/tests/Search.java
@@ -0,0 +1,19 @@
+package tests;
+
+import manage.ConfigurationWiki;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import screens.MainScreen;
+
+public class Search extends ConfigurationWiki {
+
+    @Test
+    public void search1(){
+        boolean isLiner = new MainScreen(driver)
+                .clickOnTheSearch()
+                .fillInText("dog")
+                .closeBoard()
+                .isLiner();
+        Assert.assertTrue(isLiner);
+    }
+}
